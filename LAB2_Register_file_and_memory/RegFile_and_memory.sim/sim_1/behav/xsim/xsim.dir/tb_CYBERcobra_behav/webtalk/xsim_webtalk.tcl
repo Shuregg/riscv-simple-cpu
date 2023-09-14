ee3b2806be2b3a6664538219e72e18ef1e7f2689
Alexander <san4ows55@gmail.com>
2023-09-14 19:51:26 +0300

    waveform simulation file for CYBERcobra.sv

--- LAB2_Register_file_and_memory/RegFile_and_memory.sim/sim_1/behav/xsim/xsim.dir/tb_CYBERcobra_behav/webtalk/xsim_webtalk.tcl
+++ LAB2_Register_file_and_memory/RegFile_and_memory.sim/sim_1/behav/xsim/xsim.dir/tb_CYBERcobra_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/MIET-APS-Labs/LAB2_Register_file_and_memory/RegFile_and_memory.sim/sim_1/behav/xsim/xsim.dir/tb_CYBERcobra_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Sep 14 18:35:42 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Sep 14 19:50:40 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "ae275e8f-b9c9-4dc2-b2cd-94558c6e7bd5" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "b8ba7c37795542ebba50e6658b7422a1" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "16" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "20" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "11th Gen Intel(R) Core(TM) i5-11400 @ 2.60GHz" -context "user_environment"
@@ -28,6 +28,6 @@ webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\
 webtalk_add_data -client xsim -key runtime -value "10010 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.08_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6928_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 652949628 -regid "" -xml D:/MIET-APS-Labs/LAB2_Register_file_and_memory/RegFile_and_memory.sim/sim_1/behav/xsim/xsim.dir/tb_CYBERcobra_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/MIET-APS-Labs/LAB2_Register_file_and_memory/RegFile_and_memory.sim/sim_1/behav/xsim/xsim.dir/tb_CYBERcobra_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/MIET-APS-Labs/LAB2_Register_file_and_memory/RegFile_and_memory.sim/sim_1/behav/xsim/xsim.dir/tb_CYBERcobra_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "7768_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3643720288 -regid "" -xml D:/MIET-APS-Labs/LAB2_Register_file_and_memory/RegFile_and_memory.sim/sim_1/behav/xsim/xsim.dir/tb_CYBERcobra_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/MIET-APS-Labs/LAB2_Register_file_and_memory/RegFile_and_memory.sim/sim_1/behav/xsim/xsim.dir/tb_CYBERcobra_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/MIET-APS-Labs/LAB2_Register_file_and_memory/RegFile_and_memory.sim/sim_1/behav/xsim/xsim.dir/tb_CYBERcobra_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
