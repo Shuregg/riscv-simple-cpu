b6b14c4933140004c2cb1036fe32cf69b42742f8
Alexander <san4ows55@gmail.com>
2023-10-25 18:12:31 +0300

    ready to test

--- LAB3_Main_decoder/src/decoder_riscv.sv
+++ LAB3_Main_decoder/src/decoder_riscv.sv
@@ -18,24 +18,22 @@ module decoder_riscv (
   output logic         jalr_o,
   output logic         mret_o           //signal return from interruption/exceptions mret (Machine mode RETurn)
 );
+//find "//?" to check questionable solutions
   import riscv_pkg::*;
   
   logic [6:0]   opcode;
-  logic [4:0]   rd;
+  // logic [4:0]   rd;
   logic [2:0]   funct3;
-  logic [4:0]   rs1;
-  logic [4:0]   rs2;
+  // logic [4:0]   rs1;
+  // logic [4:0]   rs2;
   logic [6:0]   funct7;
 
-  
-
-  
   always_comb begin
     opcode <= fetched_instr_i[6:0];
-    rd     <= fetched_instr_i[11:7];
+    // rd     <= fetched_instr_i[11:7];
     funct3 <= fetched_instr_i[14:12];
-    rs1    <= fetched_instr_i[19:15];
-    rs2    <= fetched_instr_i[24:20];
+    // rs1    <= fetched_instr_i[19:15];
+    // rs2    <= fetched_instr_i[24:20];
     funct7 <= fetched_instr_i[31:25];
     
     if(opcode[1:0] != 2'b11)
@@ -43,143 +41,467 @@ module decoder_riscv (
     else begin
       illegal_instr_o <= 1'b0;
       case(opcode[6:2])
+      
         OP_OPCODE: begin
           gpr_we_o <= 1'b1;
-          alu_op_o <= ALU_ADD;
-          if(funct3 == 3'b000 && funct7 == 7'b0000000) begin            //ADD
-            alu_op_o <= ALU_ADD;
-          end else
-          if(funct3 == 3'b000 && funct7 == 7'b0100000) begin            //SUB
-            alu_op_o <= ALU_SUB;
-          end else
-          if(funct3 == 3'b100 && funct7 == 7'b0000000) begin            //XOR
-            alu_op_o <= ALU_XOR;
-          end else
-          if(funct3 == 3'b110 && funct7 == 7'b0000000) begin            //OR
-            alu_op_o <= ALU_OR;
-          end else
-          if(funct3 == 3'b111 && funct7 == 7'b0000000) begin            //AND
-            alu_op_o <= ALU_AND;
-          end else
-          if(funct3 == 3'b001 && funct7 == 7'b0000000) begin            //SLL
-            alu_op_o <= ALU_SLL;
-          end else
-          if(funct3 == 3'b101 && funct7 == 7'b0000000) begin            //SRL
-            alu_op_o <= ALU_SRL;
-          end else
-          if(funct3 == 3'b101 && funct7 == 7'b0100000) begin            //SRA
-            alu_op_o <= ALU_SRA;
-          end else
-          if(funct3 == 3'b010 && funct7 == 7'b0000000) begin            //SLT
-            alu_op_o <= ALU_SLTS;
-          end else
-          if(funct3 == 3'b011 && funct7 == 7'b0000000) begin            //SLTU
-            alu_op_o <= ALU_SLTU;
-          end else begin
-            gpr_we_o <= 1'b0;
-            illegal_instr_o <= 1'b1;
-          end
-        end //end of OP_OPCODE:
-        
-        LOAD_OPCODE: begin
-          logic [11:0] imm_load;
-          imm_load <= fetched_instr_i[31:20];
+          //alu_op_o <= ALU_ADD;
           
-          if(funct3 == LDST_B) begin                                    //load bit
+          case(funct3)
+            3'b000: 
+            case(funct7)
+              7'b0000000: alu_op_o <= ALU_ADD;
+              7'b0100000: alu_op_o <= ALU_SUB;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;
+              end
+            endcase
             
-          end else
-          if(funct3 == LDST_H) begin                                    //load half
+            3'b100: 
+            case(funct7)
+              7'b0000000:  alu_op_o <= ALU_XOR;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;
+              end
+            endcase
             
-          end else
-          if(funct3 == LDST_W) begin                                    //load word
+            3'b110: 
+            case(funct7)
+              7'b0000000:  alu_op_o <= ALU_OR;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;                
+              end
+            endcase
             
-          end else
-          if(funct3 == LDST_BU) begin                                   //load byte unsigned
+            3'b111: 
+            case(funct7)
+              7'b0000000:  alu_op_o <= ALU_AND;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;                
+              end
+            endcase
             
-          end else
-          if(funct3 == LDST_HU) begin                                   //load half unsigned
+            3'b001:
+            case(funct7)
+              7'b0000000:  alu_op_o <= ALU_SLL;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;                
+              end
+            endcase
             
-          end else begin
-            //alu_op_o
-            illegal_instr_o <= 1'b1;
-          end
+            3'b101:
+            case(funct7)
+              7'b0000000:  alu_op_o <= ALU_SRL;
+              7'b0100000:  alu_op_o <= ALU_SRA;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;                
+              end
+            endcase
+            
+            3'b001:
+            case(funct7)
+              7'b0000000:  alu_op_o <= ALU_SLL;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;                
+              end
+            endcase
+            
+            3'b001:
+            case(funct7)
+              7'b0000000:  alu_op_o <= ALU_SLL;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;                
+              end
+            endcase
+            
+            3'b010:
+            case(funct7)
+              7'b0000000:  alu_op_o <= ALU_SLTS;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;                
+              end
+            endcase
+            
+            3'b011:
+            case(funct7)
+              7'b0000000:  alu_op_o <= ALU_SLTU;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;                
+              end
+            endcase
+            
+            default: begin
+              gpr_we_o        <= 1'b0;
+              mem_req_o       <= 1'b0;
+              illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;              
+            end
+          endcase
+        end //end of OP_OPCODE
+
+        LOAD_OPCODE: begin 
+          //logic [11:0] imm_U;
+          //imm_U <= fetched_instr_i[31:20];
+          gpr_we_o    <= 1'b1;      //RF_WE
+          a_sel_o     <= OP_A_RS1   //2'b0; //RF_RD1
+          b_sel_o     <= OP_B_IMM_I //2'b1;    //imm_I
+          alu_op_o    <= ALU_ADD;
+          mem_req_o   <= 1'b1;
+          mem_we_o    <= 1'b0;
+          wb_sel_o    <= WB_LSU_DATA // 2'd1; //from Load-Store Unit (data memory), not from ALU
+
+          csr_we_o  <= 1'b0;
+          jal_o     <= 1'b0;
+          jalr_o    <= 1'b0;
+          branch_o  <= 1'b0;
+
+          case(funct3)
+            LDST_B://load byte
+              mem_size_o  <= 3'd0;
+
+            LDST_H://load half
+              mem_size_o  <= 3'd1;
+
+            LDST_W://load word
+              mem_size_o  <= 3'd2;
+
+            LDST_BU://load byte unsigned
+              mem_size_o  <= 3'd4;
+
+            LDST_HU://load half unsigned
+              mem_size_o  <= 3'd5;
+            default:  begin
+              gpr_we_o        <= 1'b0;
+              mem_req_o       <= 1'b0;
+              illegal_instr_o <= 1'b1;
+              jal_o           <= 1'b0;
+              jalr_o          <= 1'b0;
+              branch_o        <= 1'b0;              
+            end
+
+          endcase  
         end //end of LOAD_OPCODE:
-        
+
         MISC_MEM_OPCODE: begin
-        
+          case(funct3)
+            3'b000:
+              //NOP
+            default: begin
+              gpr_we_o        <= 1'b0;
+              mem_req_o       <= 1'b0;
+              illegal_instr_o <= 1'b1;
+              jal_o           <= 1'b0;
+              jalr_o          <= 1'b0;
+              branch_o        <= 1'b0;              
+            end
+          endcase
         end
         
         OP_IMM_OPCODE: begin
-          logic [11:0] imm_op_imm;
-          imm_op_imm <= fetched_instr_i[31:20];
+          logic [11:0] imm_I;
+          imm_I <= fetched_instr_i[31:20];
+          gpr_we_o <= 1'b1;
           b_sel_o <= 1'b1;
-          if(funct3 == 3'b000) begin                                    //ADDI
+          case(funct3)
+            3'b000://ADDI
+              alu_op_o <= ALU_ADD;
+            3'b110://XORI
+              alu_op_o <= ALU_XOR
+            3'b110://ORI
+              alu_op_o <= ALU_OR
+            3'b111://ANDI
+              alu_op_o <= ALU_AND
+            3'b001://SLLI (Shift Left Logical Immediate)
+            case(funct7)
+              7'b0000000:
+                alu_op_o <= ALU_SLL;
+              default: begin 
+                gpr_we_o        <= 1'b0;
+                b_sel_o         <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;
+              end
+            endcase
 
-          end else
-          if(funct3 == 3'b110) begin                                    //XORI
+            3'b101:
+            case(funct7)
+              7'b0000000://SRLI (Shift Right Logic Immediate)
+                alu_op_o <= ALU_SRL;
+              7'b0100000://SRAI (Shift Right Arithmethic immediate)
+                alu_op_o <= ALU_SRA;
+              default: begin
+                gpr_we_o        <= 1'b0;
+                b_sel_o         <= 1'b0;
+                mem_req_o       <= 1'b0;
+                illegal_instr_o <= 1'b1;
+                jal_o           <= 1'b0;
+                jalr_o          <= 1'b0;
+                branch_o        <= 1'b0;                
+              end
+            endcase
 
-          end else
-          if(funct3 == 3'b110) begin                                    //ORI
+            3'b010://SLTI (Set if Less Than Immediate)
+              alu_op_o <= ALU_SLTS;
+            3'b011://SLTIU (Set if Less Than Immediate Unsigned)
+              alu_op_o <= ALU_SLTU;
+            default: begin
+              b_sel_o         <= 1'b0;
+              gpr_we_o        <= 1'b0;
+              mem_req_o       <= 1'b0;
+              illegal_instr_o <= 1'b1;
+              jal_o           <= 1'b0;
+              jalr_o          <= 1'b0;
+              branch_o        <= 1'b0;  
+            end 
+          endcase //end of OP_IMM:
+        end //end of IMM_OPCODE
 
-          end else
-          if(funct3 == 3'b111) begin                                    //ANDI
-
-          end else
-          if(funct3 == 3'b001 && funct7 == 7'b0000000) begin            //SLLI (Shift Left Logical Immediate)
-          
-
-          end else
-          if(funct3 == 3'b101 && funct7 == 7'b0000000) begin            //SRLI (Shift Right Logic Immediate)
-          
-
-          end else
-          if(funct3 == 3'b101 && funct7 == 7'b0100000) begin            //SRAI (Shift Right Arithmethic immediate)
-          
-
-          end else
-          if(funct3 == 3'b010) begin                                    //SLTI (Set if Less Than Immediate)
-          
-
-          end else
-          if(funct3 == 3'b011) begin                                    //SLTIU (Set if Less Than Immediate Unsigned)
-          
-
-          end else begin
-          
-            b_sel_o <= 1'b0;
-          end
-        end //end of OP_IMM:
-        
         AUIPC_OPCODE: begin
-        
-        end
+          gpr_we_o    <= 1'b1;
+          a_sel_o     <= OP_A_RS1;
+          b_sel_o     <= OP_B_IMM_U;
+          alu_op_i    <= ALU_ADD;
+          wb_sel_o    <= WB_EX_RESULT; //from ALU
+
+          mem_req_o   <= 1'b0;
+          branch_o    <= 1'b0;
+          jal_o       <= 1'b0;
+          jalr_o      <= 1'b0;
+        end //end of AUIPC
         
         STORE_OPCODE: begin
-        
-        end
+          gpr_we_o  <= 1'b0;
+          a_sel_o   <= OP_A_RS1;
+          b_sel_o   <= OP_B_IMM_S;
+          alu_op_o  <= ALU_ADD;
+          mem_req_o <= 1'b1;
+          mem_we_o  <= 1'b1;
+          csr_we_o  <= 1'b0;
+
+          jal_o     <= 1'b0;
+          jalr_o    <= 1'b0;
+          branch_o  <= 1'b0;
+          
+          case(funct3)
+            LDST_B: 
+              mem_size_o <= 3'd0;
+            
+            LDST_H:
+              mem_size_o <= 3'd1;
+
+            LDST_W:
+              mem_size_o <= 3'd2;
+
+            default: begin
+              gpr_we_o        <= 1'b0;
+              mem_req_o       <= 1'b0;
+              illegal_instr_o <= 1'b1;
+              jal_o           <= 1'b0;
+              jalr_o          <= 1'b0;
+              branch_o        <= 1'b0;                
+            end
+          endcase
+        end //end of STORE
         
         LUI_OPCODE: begin
-          
-        end
+          gpr_we_o  <= 1'b1;        //RF_WE
+          a_sel_o   <= OP_A_ZERO;   //2'b10    zero (const)
+          b_sel_o   <= OP_B_IMM_S;  //3'b011; signed  //instr[31:12]
+          alu_op_o  <= ALU_ADD;
+        end //end of LUI
         
         BRANCH_OPCODE: begin
-        
-        end
+          gpr_we_o  <= 1'b0;
+          branch_o  <= 1'b1;
+          a_sel_o   <= OP_A_RS1;
+          b_sel_o   <= OP_B_RS2;
+
+          jal_o     <= 1'b0; //?
+          jalr_o    <= 1'b0; //?
+          case(funct3)
+            3'b000://beq
+              alu_op_o  <= ALU_EQ;
+            3'b001://bne
+              alu_op_o  <= ALU_NE;
+            3'b100://blt
+              alu_op_o  <= ALU_LTS;
+            3'b101://bge
+              alu_op_o  <= ALU_GES
+            3'b110://bltu
+              alu_op_o  <= ALU_LTU
+            3'b111://bgeu
+              alu_op_o  <= ALU_GEU;
+            default: begin
+              branch_o        <= 1'b0;
+              gpr_we_o        <= 1'b0;
+              mem_req_o       <= 1'b0;
+              illegal_instr_o <= 1'b1;
+              jal_o           <= 1'b0;
+              jalr_o          <= 1'b0;
+              branch_o        <= 1'b0;                
+            end
+          endcase
+        end //end of BRANCH_OPCODE:
         
         JALR_OPCODE: begin
-        
-        end
+          gpr_we_o    <= 1'b1;
+          a_sel_o     <= OP_A_CURR_PC;
+          b_sel_o     <= OP_B_INCR;
+          alu_op_i    <= ALU_ADD;
+          mem_req_o   <= 1'b0;
+          wb_sel_o    <= WB_EX_RESULT; //from ALU
+
+          jal_o       <= 1'b0;
+          branch_o    <= 1'b0;
+          
+          case(funct3)
+            3'b000:
+              jalr_o  <= 1'b1;
+            default: begin
+              jalr_o          <= 1'b0;
+              gpr_we_o        <= 1'b0;
+              mem_req_o       <= 1'b0;
+              illegal_instr_o <= 1'b1;
+              jal_o           <= 1'b0;
+              jalr_o          <= 1'b0;
+              branch_o        <= 1'b0;  
+            end
+          endcase
+        end //end of JALR
         
         JAL_OPCODE: begin
+          gpr_we_o    <= 1'b1;
+          a_sel_o     <= OP_A_CURR_PC;
+          b_sel_o     <= OP_B_INCR;
+          alu_op_i    <= ALU_ADD;
+          mem_req_o   <= 1'b0;
+          wb_sel_o    <= WB_EX_RESULT; //from ALU
+          
+          jal_o       <= 1'b1;
+          branch_o    <= 1'b0;
+          jalr_o      <= 1'b0;
+        end //end of JAL
         
+        SYSTEM_OPCODE: begin //TODO
+          
+          gpr_we_o  <= 1'b1;
+          csr_we_o  <= 1'b1;
+          case(funct3)
+            3'b000: begin   //mret (machine return)
+              mret_o    <= 1'b1;
+              gpr_we_o  <= 1'b0; //?
+              csr_we_o  <= 1'b0; //?
+              case(funct7)
+                7'h00: //environment call
+                  illegal_instr_o <= 1'b1;
+                7'h01: //environment break
+                  illegal_instr_o <= 1'b1;
+                default: begin
+                  gpr_we_o        <= 1'b0;
+                  mem_req_o       <= 1'b0;
+                  illegal_instr_o <= 1'b1;
+                  jal_o           <= 1'b0;
+                  jalr_o          <= 1'b0;
+                  branch_o        <= 1'b0;                  
+                end
+              endcase
+            end
+
+            CSR_RW: begin          //= 3'b001;
+              csr_op_o  <= funct3;
+            end
+            CSR_RS: begin          //= 3'b010;
+              csr_op_o  <= funct3;
+            end
+            CSR_RC: begin          //= 3'b011;
+              csr_op_o  <= funct3;
+            end
+            CSR_RWI: begin         //= 3'b101;
+              csr_op_o  <= funct3;
+            end
+            CSR_RSI: begin         //= 3'b110;
+              csr_op_o  <= funct3;
+            end
+            CSR_RCI: begin         //= 3'b111;
+              csr_op_o  <= funct3;
+            end
+
+            default: begin
+              gpr_we_o        <= 1'b0;
+              mem_req_o       <= 1'b0;
+              illegal_instr_o <= 1'b1;
+              csr_we_o        <= 1'b0;
+              jal_o           <= 1'b0;
+              jalr_o          <= 1'b0;
+              branch_o        <= 1'b0;  
+            end
+          endcase
+        end //end of SYSTEM
+        
+        default: begin
+          gpr_we_o        <= 1'b0;
+          mem_req_o       <= 1'b0;
+          illegal_instr_o <= 1'b1;
+          csr_we_o        <= 1'b0;
+          jal_o           <= 1'b0;
+          jalr_o          <= 1'b0;
+          branch_o        <= 1'b0;  
         end
-        
-        SYSTEM_OPCODE: begin
-        
-        end
-      endcase
+      endcase //end of main case (opcode[6:2])
     end
-    
   end
-  
 endmodule
