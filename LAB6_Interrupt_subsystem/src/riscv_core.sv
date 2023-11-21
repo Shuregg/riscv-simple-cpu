885a140141cbc8b74909244167f00c3b47b30a3b
Alexander <san4ows55@gmail.com>
2023-11-21 19:53:01 +0300

    CSR/IRQ Controllers integration (not verified)

--- LAB6_Interrupt_subsystem/src/riscv_core.sv
+++ LAB6_Interrupt_subsystem/src/riscv_core.sv
@@ -20,7 +20,8 @@ module riscv_core (
 
   import riscv_pkg::*;
 
-  localparam PC_INCR = 32'd4;
+  localparam PC_INCR              = 32'd4;
+  localparam ILL_INSTR_CAUSE_CODE = 32'h0000_0002;
   
 /*===========================WIRES===========================*/
 //MAIN DECODER WIRES (ALL THIS SIGNALS ARE OUTPUT)
@@ -28,17 +29,17 @@ module riscv_core (
   logic [2:0]   MDEC_B_SEL;         //select 2nd ALU operand
   logic [4:0]   MDEC_ALU_OP;        //
   logic [2:0]   MDEC_CSR_OP;        //
-  logic         MDEC_CSR_WE;        //NC.   write enable for csr (control status register)
+  logic         MDEC_CSR_WE;        //.   write enable for csr (control status register)
   logic         MDEC_MEM_REQ;       //req mem
   logic         MDEC_MEM_WE;        //
   logic [2:0]   MDEC_MEM_SIZE;      //
   logic         MDEC_GRP_WE;        //register file write enable
   logic [1:0]   MDEC_WB_SEL;        //write back selector (RF write source)
-  logic         MDEC_ILLEGAL_INSTR; //NC.
+  logic         MDEC_ILLEGAL_INSTR; //.
   logic         MDEC_B;        
   logic         MDEC_JAL;
   logic         MDEC_JALR;
-  logic         MDEC_MRET;          //NC.    signal return from interruption/exceptions mret (Machine mode R)
+  logic         MDEC_MRET;          //.    signal return from interruption/exceptions mret (Machine mode R)
 
 //REGISTER FILE WIRES
   logic        RF_WE_IN;
@@ -61,12 +62,12 @@ module riscv_core (
   logic [31:0] imm_S;
   logic [31:0] imm_B;
   logic [31:0] imm_J;
+  logic [31:0] imm_Z;
   
 //PROGRAMM COUNTER WIRES
   logic [31:0] PC_IN;
   logic [31:0] PC_OUT;
   
-//OPTIONAL WIRES
   logic [31:0]  JALR_PC_IN;             //next PC value if MDEC_JALR == 1
   logic [31:0]  INCR_PC_IN;             //next PC value if MDEC_JALR == 0
   
@@ -75,40 +76,62 @@ module riscv_core (
   logic [31:0]  BRANCH_MUX_OUT;         //the mux controlled by MDEC_B (branch signal)
   logic [31:0]  JAL_OR_BAF_MUX_OUT;     //the mux controlled by (MDEC_JAL | (MDEC_B & ALU_FLAG_OUT)) signal (branch or jump signal)
   logic [31:0]  RD1_PLUS_imm_I;
+  //////////////////////////////
+  logic [31:0]  
+
+
+
     
+//IRQ CONTROLLER WIRES
+  logic         IRQ_CONTR_IRQ_RET_OUT
+  logic [31:0]  IRQ_CONTR_IRQ_CAUSE_OUT;
+  logic         IRQ_CONTR_IRQ_OUT;
+
+//CSR CONTROLLER WIRES
+  logic [31:0]  CSR_CONTR_MCAUSE_IN;
+
+  logic [31:0]  CSR_CONTR_MIE_OUT;
+  logic [31:0]  CSR_CONTR_MTVEC_OUT;
+  logic [31:0]  CSR_CONTR_MEPC_OUT;
+  logic [31:0]  CSR_CONTR_READ_DATA_OUT;
+
+//ADDITIONAL WIRES
+  logic         TRAP;
+  
 /*===========================ASSIGNING===========================*/ 
 //ASSIGNING (IMMEDIATE)
-assign imm_I = {{20{instr_i[31]}}, instr_i[31:20]}; //20+12=32
-assign imm_U = {instr_i[31:12], 12'h000}; //20+12=32
-assign imm_S = {{20{instr_i[31]}}, instr_i[31:25], instr_i[11:7]}; //20+12
-assign imm_B = {{20{instr_i[31]}}, instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0}; //19+1+12=32
-assign imm_J = {{12{instr_i[31]}}, instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0}; //12+1+19
+  assign imm_I = {{20{instr_i[31]}}, instr_i[31:20]}; //20+12=32
+  assign imm_U = {instr_i[31:12], 12'h000}; //20+12=32
+  assign imm_S = {{20{instr_i[31]}}, instr_i[31:25], instr_i[11:7]}; //20+12
+  assign imm_B = {{20{instr_i[31]}}, instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0}; //19+1+12=32
+  assign imm_J = {{12{instr_i[31]}}, instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0}; //12+1+19
+  assign imm_Z = {27'b0, instr_i[19:15]};
 
 //ASSIGNING (REGISTER FILE)
-assign RF_WE_IN   = !stall_i && MDEC_GRP_WE;
-assign RF_RA1_IN  = instr_i[19:15];
-assign RF_RA2_IN  = instr_i[24:20];
-assign RF_WA_IN   = instr_i[11: 7];
-assign RF_WD_IN   = MDEC_WB_SEL ? mem_rd_i : ALU_RES_OUT;
+  assign RF_WE_IN   = !(stall_i && TRAP) && MDEC_GRP_WE;
+  assign RF_RA1_IN  = instr_i[19:15];
+  assign RF_RA2_IN  = instr_i[24:20];
+  assign RF_WA_IN   = instr_i[11: 7];
+  assign RF_WD_IN   = MDEC_WB_SEL ? mem_rd_i : ALU_RES_OUT;
 
 //ASSIGNING (ALU)
-always_comb begin
-  case(MDEC_A_SEL)  //choosing operand A (mux)
-    OP_A_RS1:       ALU_A_IN <= RF_RD1_OUT;
-    OP_A_CURR_PC:   ALU_A_IN <= PC_OUT;
-    OP_A_ZERO:      ALU_A_IN <= 32'b0;
-    default: begin end
-  endcase
-  
-  case(MDEC_B_SEL) //choosing operand B (mux)
-    OP_B_RS2:       ALU_B_IN <= RF_RD2_OUT;
-    OP_B_IMM_I:     ALU_B_IN <= imm_I;
-    OP_B_IMM_U:     ALU_B_IN <= imm_U;
-    OP_B_IMM_S:     ALU_B_IN <= imm_S;
-    OP_B_INCR:      ALU_B_IN <= PC_INCR;
-    default: begin end
-  endcase
-end
+  always_comb begin
+    case(MDEC_A_SEL)  //choosing operand A (mux)
+      OP_A_RS1:       ALU_A_IN <= RF_RD1_OUT;
+      OP_A_CURR_PC:   ALU_A_IN <= PC_OUT;
+      OP_A_ZERO:      ALU_A_IN <= 32'b0;
+      default: begin end
+    endcase
+    
+    case(MDEC_B_SEL) //choosing operand B (mux)
+      OP_B_RS2:       ALU_B_IN <= RF_RD2_OUT;
+      OP_B_IMM_I:     ALU_B_IN <= imm_I;
+      OP_B_IMM_U:     ALU_B_IN <= imm_U;
+      OP_B_IMM_S:     ALU_B_IN <= imm_S;
+      OP_B_INCR:      ALU_B_IN <= PC_INCR;
+      default: begin end
+    endcase
+  end
 
 //ASSIGNING (PROGRAM COUNTER)
   assign BAF                = MDEC_B & ALU_FLAG_OUT;                    //Branch And Flag
@@ -119,19 +142,29 @@ end
 
   assign INCR_PC_IN         = PC_OUT + JAL_OR_BAF_MUX_OUT;              //next PC value if MDEC_JALR == 0
   assign RD1_PLUS_imm_I = RF_RD1_OUT + imm_I;
-  assign JALR_PC_IN = {RD1_PLUS_imm_I[31:1], 1'b0};             //next PC value if MDEC_JALR == 1
+  assign JALR_PC_IN = {RD1_PLUS_imm_I[31:1], 1'b0};                     //next PC value if MDEC_JALR == 1
   
-  assign PC_IN              = MDEC_JALR ? JALR_PC_IN : INCR_PC_IN;      //next PC value
+  
+  assign MTVEC_PC_IN  = TRAP ? CSR_CONTR_MTVEC_OUT : JALR_PC_IN;
+  assign PC_IN   = MDEC_MRET ? CSR_CONTR_MEPC_OUT : MTVEC_PC_IN;
+  // assign PC_IN              = MDEC_JALR ? JALR_PC_IN : INCR_PC_IN;      //next PC value
 
-//ASSIGNGING (RISCV CORE)
-assign instr_addr_o = PC_OUT; 
-assign mem_addr_o   = ALU_RES_OUT;
-/*3'd0 (load byte); 3'd1 (load half); 3'd2 (load word); 3'd4 (load byte unsigned); 3'd5 (load half unsigned).*/
-assign mem_size_o   = MDEC_MEM_SIZE; 
-assign mem_req_o    = MDEC_MEM_REQ;
-assign mem_we_o     = MDEC_MEM_WE;
-assign mem_wd_o     = RF_RD2_OUT;
+//ASSIGNING (RISCV CORE)
+  assign instr_addr_o = PC_OUT; 
+  assign mem_addr_o   = ALU_RES_OUT;
+    /*3'd0 (load byte); 3'd1 (load half); 3'd2 (load word); 3'd4 (load byte unsigned); 3'd5 (load half unsigned).*/
+  assign mem_size_o   = MDEC_MEM_SIZE; 
+  assign mem_req_o    = MDEC_MEM_REQ;
+  assign mem_we_o     = MDEC_MEM_WE;
+  assign mem_wd_o     = RF_RD2_OUT;
   
+//ASSIGNING (ADDITIONAL WIRES)
+  assign TRAP = MDEC_ILLEGAL_INSTR || IRQ_CONTR_IRQ_OUT;
+
+//ASSIGNING (CSR CONTROLLER WIRES)
+  assign CSR_CONTR_MCAUSE_IN = MDEC_ILLEGAL_INSTR ? ILL_INSTR_CAUSE_CODE : IRQ_CONTR_IRQ_CAUSE_OUT;
+
+
 /*===========================INSTANCES===========================*/ 
 //MAIN DECODER INSTANCE
   decoder_riscv main_decoder_inst 
@@ -141,17 +174,17 @@ assign mem_wd_o     = RF_RD2_OUT;
     .b_sel_o(MDEC_B_SEL),
     .alu_op_o(MDEC_ALU_OP),        
     .csr_op_o(MDEC_CSR_OP),
-    .csr_we_o(MDEC_CSR_WE),                 //NC
+    .csr_we_o(MDEC_CSR_WE),                 //.
     .mem_req_o(MDEC_MEM_REQ),
     .mem_we_o(MDEC_MEM_WE),        
     .mem_size_o(MDEC_MEM_SIZE),      
     .gpr_we_o(MDEC_GRP_WE),
     .wb_sel_o(MDEC_WB_SEL),
-    .illegal_instr_o(MDEC_ILLEGAL_INSTR),   //NC
+    .illegal_instr_o(MDEC_ILLEGAL_INSTR),   //.
     .branch_o(MDEC_B),        
     .jal_o(MDEC_JAL),
     .jalr_o(MDEC_JALR),
-    .mret_o(MDEC_MRET)                     //NC    
+    .mret_o(MDEC_MRET)                      //.
   );
   
 //REGISTER FILE INSTANCE
@@ -186,4 +219,41 @@ assign mem_wd_o     = RF_RD2_OUT;
     .pc_i(PC_IN),
     .pc_o(PC_OUT)
   );
+
+//IRQ CONTROLLER INSTANCE
+  irq_controller  irq_controller_inst
+  (
+    .clk_i(clk_i),
+    .rst_i(rst_i),
+    .exception_i(MDEC_ILLEGAL_INSTR),
+    .irq_req_i(irq_req_i),
+    .mie_i(CSR_CONTR_MIE_OUT[0]),
+    .mret_i(MDEC_MRET),
+
+    .irq_ret_o(IRQ_CONTR_IRQ_RET_OUT),
+    .irq_cause_o(IRQ_CONTR_IRQ_CAUSE_OUT),  //[31:0]
+    .irq_o(IRQ_CONTR_IRQ_OUT)
+  );
+
+//CSR CONTROLLER INTANCE
+  csr_controller csr_controller_inst
+  (
+  .clk_i(clk_i),
+  .rst_i(rst_i),
+  .trap_i(TRAP),
+
+  .opcode_i(MDEC_CSR_OP),        //[ 2:0]
+
+  .addr_i(instr_i[31:20]),          //[11:0]
+  .pc_i(PC_OUT),            //[31:0]
+  .mcause_i(CSR_CONTR_MCAUSE_IN),        //[31:0]
+  .rs1_data_i(RF_RD1_OUT),      //[31:0]
+  .imm_data_i(imm_Z),      //[31:0]
+  .write_enable_i(MDEC_CSR_WE),  //[31:0]
+
+  .read_data_o(CSR_CONTR_READ_DATA_OUT),     //[31:0]
+  .mie_o(CSR_CONTR_MIE_OUT),           //[31:0]
+  .mepc_o(CSR_CONTR_MEPC_OUT),          //[31:0]
+  .mtvec_o(CSR_CONTR_MTVEC_OUT)          //[31:0]
+  );  
 endmodule
